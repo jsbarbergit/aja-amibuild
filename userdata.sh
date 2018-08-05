231a131625e4c8d04582055e2d1a54816b5ac94a
John Barber <jsbarber@gmail.com>
2018-08-05 18:36:43 +0100

    added launch scripts

--- userdata.sh
+++ userdata.sh
@@ -3,5 +3,7 @@ sudo yum install -y pure-ftpd
 sudo chkconfig --add pure-ftpd
 sudo chkconfig --level 235  pure-ftpd on
 # TO DO add config file
+echo
 sudo chmod +x /etc/rc.local
 sudo echo "pure-uploadscript -B -r /opt/uploadscript.sh" >> /etc/rc.d/rc.local
+sudo mv -f /tmp/pure-ftpd.conf /etc/pure-ftpd/pure-ftpd.conf
